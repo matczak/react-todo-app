708e16c9af21b6d6aabca4bfc2bb8ac99058612f
michnik <michnik13@gmail.com>
2019-07-30 22:24:37 +0200

    First test has been added

---
+++ src/components/Input/Input.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Input from './Input';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Input />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should call "addTodo"', async () => {
+    const fn = jest.fn();
+    const input = mount(
+        <Input addTodo={fn}/>
+    );
+
+    input.find('input').simulate('change', { target: { value: 'TODO' } });
+    input.find('form').simulate('submit');
+
+    expect(fn).toBeCalledWith('TODO');
+});
